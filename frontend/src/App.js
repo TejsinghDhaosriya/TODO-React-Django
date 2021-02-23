483d3bb1726d182a9c327f312bcbc06586c6cdaf
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-23 17:23:34 +0530

    [FE] - removed react-router state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Todos from './components/Todos/index'
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
