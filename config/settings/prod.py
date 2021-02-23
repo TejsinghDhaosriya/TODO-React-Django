b383c3d6adbd6299442f9c4a4e898c9b8f6a0aad
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-23 13:43:29 +0530

    heroku allow

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -17,3 +17,4 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+ALLOWED_HOSTS = ["tej-todo-backend.herokuapp.com"]
