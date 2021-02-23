50f4086e3f718306d8bbd0219b71122b5ee9ac0e
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-23 13:36:57 +0530

    heroku filter dix

--- config/settings/base.py
+++ config/settings/base.py
@@ -114,7 +114,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ['django_filters.rest_framework.DjangoFilterBackend',],
     'DEFAULT_AUTHENTICATION_CLASSES': [
     # 'rest_framework.authentication.SessionAuthentication',
     'rest_framework.authentication.BasicAuthentication',
