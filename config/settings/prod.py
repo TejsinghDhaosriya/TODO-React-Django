132f86606bc7c7e9cf07cd83630ea29ea74cc1e2
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-23 13:50:33 +0530

    heroku allow

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,6 +2,16 @@ import dj_database_url
 from .base import *
 DEBUG = True
 WSGI_APPLICATION = 'config.wsgi.prod.application'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'root',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
