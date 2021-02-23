5d8c2a7b7d9be2d44420def5f3ab46640e0b20cd
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-23 13:38:31 +0530

    heroku filter dix

--- todos/api/views.py
+++ todos/api/views.py
@@ -4,15 +4,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from .serializers import TodoSerializer
 from ..models import Todo
-import django_filters
 
 
-# class TodoFilter(django_filters.FilterSet):
-#     # min_from = django_filters.NumberFilter(name="todo_from", lookup_type='gte')
-#     # max_from = django_filters.NumberFilter(name="todo_from", lookup_type='lte')
-#     class Meta:
-#         model = Todo
-#         fields = ['is_active']
 
 
 
@@ -20,7 +13,6 @@ class TodoViewSet(ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
     filter_fields = ['is_active','date']
-    # filter_class = TodoFilter
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
