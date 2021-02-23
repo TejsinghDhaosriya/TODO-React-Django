ff93f07d731a919bcfbb3aa22472c3b1c1c2c1d8
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-23 14:26:02 +0530

    heroku allow

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 import dj_database_url
 from .base import *
-DEBUG = False
+DEBUG = True
 WSGI_APPLICATION = 'config.wsgi.prod.application'
 DATABASES = {
     'default': {
--- frontend/src/components/Todos/api.js
+++ frontend/src/components/Todos/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const getTodosListing = (params) => axios.get(`https://tej-todo-backend.herokuapp.com/api/todos`, { params });
 export const postTodoAdd = (data) => axios.post(`https://tej-todo-backend.herokuapp.com/api/todos/`, data);
-export const putTodoUpdate = (data, id) => axios.put(`/api/todos/${id}/`, data);
-export const deleteTodoDelete = (param) => axios.delete(`/api/todos/${param}`);
+export const putTodoUpdate = (data, id) => axios.put(`https://tej-todo-backend.herokuapp.com/api/todos/${id}/`, data);
+export const deleteTodoDelete = (param) => axios.delete(`https://tej-todo-backend.herokuapp.com/api/todos/${param}`);
