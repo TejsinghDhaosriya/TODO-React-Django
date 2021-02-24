48cd76cf7e3a09c3a221427c1b793dcb62aa3fca
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-02-24 12:41:55 +0530

    localhost host to heroku fix

--- frontend/src/components/Todos/api.js
+++ frontend/src/components/Todos/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://tej-todo-backend.herokuapp.com/"
+const baseURL = "https://tej-todo-backend.herokuapp.com"
 export const getTodosListing = (params) => axios.get(`${baseURL}/api/todos`, { params });
 export const postTodoAdd = (data) => axios.post(`${baseURL}/api/todos/`, data);
 export const putTodoUpdate = (data, id) => axios.put(`${baseURL}/api/todos/${id}/`, data);
